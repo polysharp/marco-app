719703610d1bf362fd3a75aef87a7185bb51ae8c
Henri Fardeau <henri.fardeau@outlook.fr>
2020-04-25 20:16:43 +0200

    feat: implementing tailwind

--- src/Spinner.jsx
+++ src/Spinner.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './loading.css';
+import './styles/loading.css';
 
 const Spinner = () => (
   <div className="lds-grid">
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import './global.css';
+import './styles/tailwind.generated.css';
+import './styles/global.css';
 
 ReactDOM.render(
   <React.StrictMode>
