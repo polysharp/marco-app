3139233df59f75606b4f1bcb37cc8a446eb25a98
Henri Fardeau <henri.fardeau@outlook.fr>
2020-04-26 16:35:28 +0200

    fix: name of null error

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -64,7 +64,6 @@ const Search = observer(() => {
         {showCities &&
           data &&
           data.length > 0 &&
-          data.name &&
           data.map((el, index) => (
             <button key={index} type="button" onClick={() => handleCitySelect(el)}>
               {`${el.name}(${el.zip[0]})`}
